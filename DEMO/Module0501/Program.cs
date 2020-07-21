b24632541efe5600aec280ae6bd3d3ae347ecba4
Money Yu <abc12207@gmail.com>
2020-07-21 09:55:02 +0800

    add az400 demo

---
+++ DEMO/Module0501/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module0501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
