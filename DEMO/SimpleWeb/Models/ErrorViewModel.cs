092f58ef64d68143fefb986adad9bdae177557dc
Money Yu <abc12207@gmail.com>
2021-09-10 21:47:07 +0800

    add pulumi project

--- DEMO/SimpleWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Module05.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
